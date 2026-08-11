26f4aeb9762e7b568cc8792468585a09697f9822
William Jeffreys <wjeffreys96@gmail.com>
2026-08-10 21:44:37 -0700

    Use probe-rs for flashing
    This adds configuration to the board.cmake files for both boards to
    enable probe-rs usage for flashing. It sets the default flasher to
    probe-rs and the default debug tool to probe-rs. It also adds a Flashing
    field to the readme.

--- boards/oresat_stm32_battery_card/board.cmake
+++ boards/oresat_stm32_battery_card/board.cmake
@@ -4,7 +4,10 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32F091RC" "--speed=4000")
 
-# keep first
+# Probe-RS support
+board_runner_args(probe-rs "--chip=STM32F091RC")
+
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
