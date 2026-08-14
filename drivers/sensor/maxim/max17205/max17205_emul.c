3d140e7a822465bf2ccc6e32211ab731a46363b7
William Jeffreys <wjeffreys96@gmail.com>
2026-08-14 00:21:58 +0000

    Add helper functions to decode/encode register data
    These functions take in an expected value and convert it to the register
    value that the driver will expect to see. They are the inverse functions
    of the functions found in the driver code.

--- drivers/sensor/maxim/max17205/max17205_emul.c
+++ drivers/sensor/maxim/max17205/max17205_emul.c
@@ -15,16 +15,65 @@ LOG_MODULE_REGISTER(max17205_emul, CONFIG_EMUL_LOG_LEVEL);
 
 #define DT_DRV_COMPAT maxim_max17205
 
-static uint8_t const soc_percent_init = 75;
-static uint16_t const vbat_mV_init = 7400;
-static uint16_t const cycles_init = 12;
-static int16_t const current_val_init = 100;
-static int16_t const temp_val_init = (25 * 256);
+static inline uint16_t maH_to_raw(uint16_t rsense_mohms, uint32_t dest_mAh) {
+  return (uint16_t)((dest_mAh * rsense_mohms * 1000U) / 5000U);
+}
+
+static inline uint16_t percentage_to_raw(uint16_t perc) { return 256U * perc; }
+
+static inline uint16_t cycles_to_raw(uint16_t cycles) {
+  return (uint16_t)(100U * cycles / 16U);
+}
+
+static inline uint16_t batt_voltage_to_raw(uint16_t mV) {
+  return (uint16_t)((100U * mV) / 125U);
+}
+static inline uint16_t voltage_to_raw(uint16_t mV) {
+  return (uint16_t)((64U * mV) / 5U);
+}
+static inline uint16_t min_max_voltage_to_raw(uint32_t min_mV,
+                                              uint32_t max_mV) {
+  if (min_mV > 5100) {
+    LOG_WRN("clamping min voltage to 5100");
+    min_mV = 5100;
+  }
+  if (max_mV > 5100) {
+    LOG_WRN("clamping max voltage to 5100");
+    max_mV = 5100;
+  }
+
+  uint8_t min_raw = (uint8_t)(min_mV / 20);
+  uint8_t max_raw = (uint8_t)(max_mV / 20);
+
+  return (uint16_t)(min_raw | (max_raw << 8));
+}
+
+static inline int16_t current_to_raw(uint16_t rsense_mohms, int16_t mA) {
+  int64_t ret = ((int64_t)mA * ((int32_t)rsense_mohms * 10000)) / 15625;
+
+  return (int16_t)(ret);
+}
+
+static inline uint16_t temp_to_raw(int32_t temp) {
+  return (uint16_t)(256 * temp / 1000);
+}
+
+static inline int16_t avg_temp_to_raw(int16_t avg_temp) {
+  int16_t raw = (int16_t)((avg_temp + 273) * 10);
+  int16_t temp = (int16_t)raw / 10 - 273;
+  return raw;
+}
+
+static inline uint16_t time_to_raw(uint32_t time) {
+  return (uint16_t)(time * 1000U / 5625U);
+}
 
 /** Static configuration for the emulator */
 struct max17205_emul_cfg {
   /** I2C address of emulator */
   uint16_t addr;
+  /* Value of Rsense resistor in milliohms (typically 5 or 10) */
+  uint16_t rsense_mohms;
 };
 
 struct max17205_emul_data {
